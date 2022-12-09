8f5e3aa16891bab1632bb3dcc4b720231883e919
rbeazley18 <robertbeazley18@gmail.com>
2022-12-09 09:04:49 -0500

    last

--- server/db/conn.js
+++ server/db/conn.js
@@ -37,7 +37,7 @@ async function main() {
     }
 }
 
-main().catch(console.error);
+
 
 async function listDatabases(client) {
     databasesList = await client.db().admin().listDatabases();
@@ -45,3 +45,5 @@ async function listDatabases(client) {
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
+
+main().catch(console.error);
